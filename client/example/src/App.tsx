9833a471238ce71babfb6764d64adbb9c2f2ac27
Peter Chang <peter.chang@diamond.ac.uk>
2023-11-23 08:57:16 +0000

    Use new component and fix start script

--- client/example/src/App.tsx
+++ client/example/src/App.tsx
@@ -4,14 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import { PComponent, PComponentProps } from '@peter/components';
+import { QComponent, QComponentProps } from '@peter/components';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const props: PComponentProps = {
+  const pProps: PComponentProps = {
     text: `Clicked ${count} times`,
     handleClick: () => setCount((c) => c + 1),
   };
+
+  const qProps: QComponentProps = {
+    qText: `Clicked ${count} times`,
+    qHandleClick: () => setCount((c) => c - 1),
+  };
+
   return (
     <>
       <div>
@@ -24,10 +31,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <PComponent {...props} />
+        <PComponent {...pProps} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <QComponent {...qProps} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
