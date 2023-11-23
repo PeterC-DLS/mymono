5033703f1a9c74e9a1097579c1f3c44a45ac31ba
Peter Chang <peter.chang@diamond.ac.uk>
2023-11-23 11:27:37 +0000

    Add storybook with stories for PComponent

---
+++ storybook/stories/PComponent.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+import { PComponent, PComponentProps } from '@peter/components';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Example/Button',
+  component: PComponent,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // text: { control: 'color' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Static: Story = {
+  args: {
+    text: "Hello world",
+  },
+};
+
+const ComponentWithHooks = () => {
+    const [count, setCount] = useState<number>(0);
+    const props: PComponentProps = {
+        text: `Count is ${count}`,
+        handleClick: () => setCount((c: number) => c+1)
+    }
+    return <PComponent {...props} />;
+};
+
+
+export const Dynamic: StoryObj<typeof ComponentWithHooks> = {
+    render: () => <ComponentWithHooks />,
+};
